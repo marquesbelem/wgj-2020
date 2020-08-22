cd0473febdc5f0345c1755ceb1d98475477bb220
marquesbelem <camilambdasilva@gmail.com>
2020-08-22 19:08:44 -0300

    movimentação inicial do personagem

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject Earth;
+    public float Speed;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var position = new Vector3(vertical, 0, -horizontal);
+
+        transform.RotateAround(Earth.transform.position, position, Speed * Time.deltaTime);
+        Earth.transform.Rotate(position);
+    }
+}
