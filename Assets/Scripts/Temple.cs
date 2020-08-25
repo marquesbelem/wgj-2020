157b6d682b2618af4751edb93cf3ab129166201d
marquesbelem <camilambdasilva@gmail.com>
2020-08-25 10:43:15 -0300

    no message

---
+++ Assets/Scripts/Temple.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Temple : MonoBehaviour
+{
+    public enum TempleType { Solar, Night };
+    public TempleType TypeTemple;
+
+    public int TotalCountResources;
+    public int CurrentResources;
+    public List<GameObject> Crystal;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void UptadeCountResources()
+    {
+        CurrentResources = transform.childCount;
+        Crystal[CurrentResources - 1].SetActive(true);
+
+        if (CurrentResources == TotalCountResources)
+        {
+            if (TypeTemple == TempleType.Solar)
+            {
+                foreach (GameObject c in Crystal)
+                    c.tag = "SolarCrystal";
+            }
+            if (TypeTemple == TempleType.Night)
+            {
+                foreach (GameObject c in Crystal)
+                    c.tag = "NightCrystal";
+            }
+        }
+    }
+}
