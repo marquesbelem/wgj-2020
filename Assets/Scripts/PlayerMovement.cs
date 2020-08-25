f76d258d29b7317ecd93d92a795012181debca18
marquesbelem <camilambdasilva@gmail.com>
2020-08-25 12:10:09 -0300

    criando uma cena para o game, arrumando a movimentação do personagem e dando flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float Speed;
     public float SpeedRotation;
     private GameObject _Camera;
+    public GameObject Armature;
     private void Start()
     {
         _Camera = GameObject.FindGameObjectWithTag("MainCamera");
@@ -14,13 +16,22 @@ public class PlayerMovement : MonoBehaviour
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
-        var position = new Vector3(vertical, 0, -horizontal);
+        var position = new Vector3(-(float)Math.Round(vertical), 0, (float)Math.Round(horizontal));
 
-        transform.RotateAround(Earth.transform.position, position, Speed * Time.deltaTime);
-        _Camera.transform.RotateAround(Earth.transform.position, position, Speed * Time.deltaTime);
+        // transform.RotateAround(Earth.transform.position, position, Speed * Time.deltaTime);
+        // _Camera.transform.RotateAround(Earth.transform.position, position, Speed * Time.deltaTime);
 
         var rotationEarth = position * SpeedRotation * Time.deltaTime;
         Earth.transform.Rotate(rotationEarth);
 
+
+        if (Math.Round(horizontal) == -1)
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        if (Math.Round(horizontal) == 1)
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        if (Math.Round(vertical) == -1)
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        if (Math.Round(vertical) == 1)
+            transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
