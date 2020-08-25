837e31c5cce225b6427ca880e4ec8ed5cbe76987
marquesbelem <camilambdasilva@gmail.com>
2020-08-25 12:12:25 -0300

    removendo lixo do codigo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,15 @@ public class PlayerMovement : MonoBehaviour
     public GameObject Earth;
     public float Speed;
     public float SpeedRotation;
-    private GameObject _Camera;
-    public GameObject Armature;
-    private void Start()
-    {
-        _Camera = GameObject.FindGameObjectWithTag("MainCamera");
-    }
+   
     void Update()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         var position = new Vector3(-(float)Math.Round(vertical), 0, (float)Math.Round(horizontal));
 
-        // transform.RotateAround(Earth.transform.position, position, Speed * Time.deltaTime);
-        // _Camera.transform.RotateAround(Earth.transform.position, position, Speed * Time.deltaTime);
-
         var rotationEarth = position * SpeedRotation * Time.deltaTime;
         Earth.transform.Rotate(rotationEarth);
-
 
         if (Math.Round(horizontal) == -1)
             transform.rotation = Quaternion.Euler(0, -90, 0);
