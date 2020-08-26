aef6f895a265473f66e2fc9f9eec92161fc54e2f
marquesbelem <camilambdasilva@gmail.com>
2020-08-26 17:35:52 -0300

    Logica de troca de ciclo e sons

--- Assets/Scripts/PlayerCollect.cs
+++ Assets/Scripts/PlayerCollect.cs
@@ -6,13 +6,19 @@ public class PlayerCollect : MonoBehaviour
 {
     public enum Player { Solar, Night };
     public Player PlayerType;
+
+    private int _TotalCountResourcesSolar;
+    private int _TotalCountResourcesNight;
+
+    private int _CurrentCountResourcesSolar = 0;
+    private int _CurrentCountResourcesNight = 0;
     void OnTriggerEnter(Collider col)
     {
         if (PlayerType == Player.Solar)
         {
             if (col.gameObject.CompareTag("SolarCrystal"))
             {
-                Debug.Log("Cristal Solar");
+                ActionSolar();
                 Destroy(col.gameObject);
             }
         }
@@ -21,10 +27,39 @@ public class PlayerCollect : MonoBehaviour
         {
             if (col.gameObject.CompareTag("NightCrystal"))
             {
-                Debug.Log("Noturno");
+                ActionNight();
                 Destroy(col.gameObject);
             }
         }
     }
+
+    void ActionSolar()
+    {
+        var temple = GameObject.FindGameObjectWithTag("TempleSolar").GetComponent<Temple>();
+        _TotalCountResourcesSolar = temple.TotalCountResources;
+
+        _CurrentCountResourcesSolar++;
+
+        if(_CurrentCountResourcesSolar>= _TotalCountResourcesSolar)
+        {
+            GameManager.Instance.EnableObjsNight(true);
+            GameManager.Instance.EnableObjsSolar(false);
+            GameManager.Instance.ChangeSkybox(0);
+        }
+    }
+    void ActionNight()
+    {
+        var temple = GameObject.FindGameObjectWithTag("TempleNight").GetComponent<Temple>();
+        _TotalCountResourcesNight = temple.TotalCountResources;
+
+        _CurrentCountResourcesNight++;
+
+        if (_CurrentCountResourcesNight >= _TotalCountResourcesNight)
+        {
+            //Encontro das players
+            GameManager.Instance.ChangeSkybox(1);
+        }
+    }
+
 
 }
