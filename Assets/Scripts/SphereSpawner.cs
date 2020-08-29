a0c5b91c4ff3823ad4f87ec99e27cc0888ea7851
DESKTOP-BQ1506H\Alice <alice.duarte13@gmail.com>
2020-08-28 22:05:58 -0300

    grama spawn

---
+++ Assets/Scripts/SphereSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+    public float radius = 1;
+    public int quantidade = 100;
+    public List<GameObject> spawnable;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        for(int i = 0;i < quantidade; i++)
+        {
+            Vector3 direcao = Random.insideUnitSphere.normalized;
+            Transform novo = Instantiate(spawnable[Random.Range(0, spawnable.Count)]).transform;
+            novo.position = transform.position + direcao.normalized * radius;
+            novo.up = direcao;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
