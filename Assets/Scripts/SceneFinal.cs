c094c9d7b6810baabf341513faf12b8ae84ab9f1
marquesbelem <camilambdasilva@gmail.com>
2020-08-30 11:38:19 -0300

    ajuste no menu, colocando o tutorial, creditos no menu e no final, arrumado a coleta das gemas

---
+++ Assets/Scripts/SceneFinal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFinal : MonoBehaviour
+{
+    public GameObject Canvas;
+    void Start()
+    {
+        StartCoroutine(EnableCanvas());
+    }
+
+    IEnumerator EnableCanvas()
+    {
+        yield return new WaitForSeconds(16f);
+        Canvas.SetActive(true);
+        yield return new WaitForSeconds(16f);
+        SceneManager.LoadScene("Menu");
+    }
+}
